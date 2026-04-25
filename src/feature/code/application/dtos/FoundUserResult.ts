f4e290d54263f0c296ef76de0f0e246701dd5878
dsave-GX4 <angoga344@gmail.com>
2026-04-25 01:31:33 -0600

    Actualizacion en busqueda de amigo para logica de negocio en app

--- src/feature/code/application/dtos/FoundUserResult.ts
+++ src/feature/code/application/dtos/FoundUserResult.ts
@@ -3,4 +3,7 @@ export default interface FoundUserResult {
     name: string;
     avatarUrl?: string;
     code: string;
+    requestId?: string;
+    requestStatus?: 'pending' | 'rejected' | 'accepted';
+    isRequester?: boolean;    // true = yo la envié, false = me la enviaron a mí
 }
