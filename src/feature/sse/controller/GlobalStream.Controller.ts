01feb498114b644e7546ffba69f9faf75f9676fb
dsave-GX4 <angoga344@gmail.com>
2026-04-27 11:00:12 -0600

    Modificacion en sse nuevo

--- src/feature/sse/controller/GlobalStream.Controller.ts
+++ src/feature/sse/controller/GlobalStream.Controller.ts
@@ -13,7 +13,7 @@ export default class GlobalSseManager implements IGlobalConnectionManager, Frien
         }
         
         this.clients.get(userId)!.add(res);
-        console.log(`[Global SSE] Cliente conectado. Usuario: ${userId}. Conexiones activas: ${this.clients.get(userId)!.size}`);
+        console.log(`[Global SSE] Cliente conectado. Usuario: ${userId}. Conexiones activas : ${this.clients.get(userId)!.size}`);
     }
 
     removeClient(userId: string, res: Response): void {
@@ -21,7 +21,7 @@ export default class GlobalSseManager implements IGlobalConnectionManager, Frien
         
         if (userClients) {
             userClients.delete(res);
-            console.log(`[Global SSE] Cliente desconectado. Usuario: ${userId}. Conexiones restantes: ${userClients.size}`);
+            console.log(`[Global SSE] Cliente desconectado. Usuario: ${userId}. Conexiones restantes : ${userClients.size}`);
             
             if (userClients.size === 0) {
                 this.clients.delete(userId);
